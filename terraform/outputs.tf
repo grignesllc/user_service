cc4b74139354a9fe4d820b822ead187683c3a975
Your Name <you@example.com>
2025-03-01 21:35:20 -0500

    terrafrom & k8s

---
+++ terraform/outputs.tf
@@ -0,0 +1,15 @@
+output "eks_cluster_id" {
+  value = module.eks.cluster_id
+}
+
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
